344e8b80fb4cf6d22884a2114958af65ab3aa454
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-08-22 22:18:58 +0530

    alignments

--- src/components/ProjectsMarquee.js
+++ src/components/ProjectsMarquee.js
@@ -15,7 +15,7 @@ const projects = [
 
 const MarqueeProjects = () => {
     return (
-        <div className="relative w-full overflow-hidden" style={{ marginTop: "-100px" }}>
+        <div className="relative w-full overflow-hidden py-10 -mt-80 sm:-mt-60 md:-mt-60 lg:mt-10 xl:-mt-20">
             {/* Left fade gradient */}
             <div className="absolute left-0 top-0 bottom-0 w-24 bg-gradient-to-r from-white via-white/90 to-transparent z-10 pointer-events-none"></div>
 
