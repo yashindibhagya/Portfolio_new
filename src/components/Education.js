5c8ab82a0334bc26dff7a39c84020b8c1274a4aa
Yashindi Bhagya <yashindi@mobelo.dev>
2025-12-27 12:09:08 +0530

    education update

--- src/components/Education.js
+++ src/components/Education.js
@@ -23,6 +23,12 @@ const NeumorphicToggleSection = () => {
     // Experience data
     const experienceData = [
         {
+            role: "Junior Mobile Application Developer",
+            place: "BridgeMe AB",
+            period: "2025 Dec - Present",
+            work: "Remote"
+        },
+        {
             role: "UI/UX Designer - Intern",
             place: "Amerck Inc",
             period: "2025 May - 2025 Aug",
